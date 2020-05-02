c5d470a7205379f7da74f52f862c650ab958c16e
icarus404 <6576@ait.nsw.edu.au>
2020-05-02 18:30:42 +1000

    todo next: functions for task done and delete

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,92 +1,79 @@
+var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 
 
-renderTodoList();
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+ var app = {
+  
+//     // Application Constructor
+    initialize: function () {
+        document.addEventListener('deviceready', this.whenDeviceReady.bind(this),false);
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        document.addEventListener('pause', app.onPause);
-        document.addEventListener('resume', app.onResume);
-        app.receivedEvent('deviceready');
-
-        // this.receivedEvent('deviceready');
-    },
-
-    onPause: function () {
-        console.log("Device on pause");
       },
+
+//     // deviceready Event Handler
+//     //
+//     // Bind any cordova events here. Common events are:
+//     // 'pause', 'resume', etc.
+    whenDeviceReady: function() {
+      alert("Your app is responding!");
+         document.addEventListener('pause', this.onPause);
+         document.addEventListener('resume', this.onResume);
+         this.receivedEvent('deviceready');
+
+
+     },
+
+      onPause: function () {
+          console.log("Device on pause");
+       },
       onResume: function () {
-        // console.log("Device on resume");
+        
         alert("Welcome back!");
       },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
+//     // Update addTaskToList on a Received Event
+     receivedEvent: function() {
+      
+      
+     }
+ };
 
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
+ app.initialize();
 
-        console.log('Received Event: ' + id);
-    }
-};
 
-app.initialize();
-
-document.getElementById('addTask').addEventListener('click', function () {
-    var value = document.getElementById('inputTaskName').value;
-    if (value) {
-      addTask(value);
-    }
-  });
+    // If there is any text inside the item field, add that text to the todo list
+document.getElementById('addButton').addEventListener('click', 
+function () {
   
-  document.getElementById('inputTaskName').addEventListener('keydown', function (e) {
-    var value = this.value;
-    if ((e.code === 'Enter' || e.code === 'NumpadEnter') && value) {
-      addTask(value);
-    }
-  });
-  
-  function addTask(value) {
-    addTaskToList(value);
-    document.getElementById('inputTaskName').value = '';
-  
-    data.todo.push(value);
-    //dataObjectUpdated();
-  };
+  var inputTaskName = document.getElementById('inputTaskName').value;
+  alert("Your add button is clicking!");
 
+var list = document.getElementById("todo")
 
-  // Adding a new Task to the list
-function addTaskToList(text, completed) {
-    var list = document.getElementById('todo');
-  
-    var inputTaskName = document.createElement('li');
-    inputTaskName.innerText = text;
-  
-    var buttons = document.createElement('div');
-    buttons.classList.addTask('buttons');
-  
-    var remove = document.createElement('button');
-    remove.classList.addTask('remove');
-    remove.innerHTML = removeSVG;
+var item = document.createElement("li")
+   item.innerHTML = inputTaskName;
 
-    list.insertBefore(inputTaskName, list.childNodes[0]);
-}
+var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
 
-function renderTodoList() {
-   
-  for (var i = 0; i < data.todo.length; i++) {
-    var value = data.todo[i];
-    addTaskToList(value);
-  }
-  }
+  var remove = document.createElement('button');
+  remove.classList.add('remove');
+  remove.innerHTML = removeSVG;
+
+  // Add click event for removing the item
+  remove.addEventListener('click', removeItem);
+
+  var complete = document.createElement('button');
+  complete.classList.add('complete');
+  complete.innerHTML = completeSVG;
+
+  // Add click event for completing the item
+  complete.addEventListener('click', completeItem);
+
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  item.appendChild(buttons);
+list.appendChild(item);
+
+});
